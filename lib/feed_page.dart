1387024a52aacbfc80ff036f32a2ddf8d0027665
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-10 11:40:02 +0200

    Update feed_page.dart

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -129,18 +129,6 @@ class _FeedPageState extends State<FeedPage> {
       }
     }
 
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    if (userFeedSub != null) userFeedSub.cancel();
-    if (mainFeedSub != null) mainFeedSub.cancel();
-    if (notificationsSub != null) notificationsSub.cancel();
-
-    if (userSub != null) {
-      userSub.cancel();
-
-      dp.removeProfileStream(widget.userId, localId);
-    }
     // _controller.dispose();
 
     super.dispose();
