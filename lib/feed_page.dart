8c6abf7348d076be29a9ad7079e280a62fb051db
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-13 16:44:59 +0200

    Update feed_page.dart

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -456,6 +456,20 @@ class _FeedPageState extends State<FeedPage> {
             return Text(widget.userId.toString());
           }
            */
+
+                              setState(() {
+                                _isProcessingBatchAction = false;
+                              });
+                            }
+                          },
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              value: 'acceptAll',
+                              child: Text('Accept all followers'),
+                            ),
+                          ],
+                        ),
                   /*  RaisedButton(
                     child: Text('Accept all'),
                     onPressed: () async {
