2c97481bdaec5c24944bfb11701a3837b50c80a5
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-13 16:44:16 +0200

    Update feed_page.dart

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -457,15 +457,6 @@ class _FeedPageState extends State<FeedPage> {
           }
            */
 
-          if (isMobile) {
-            if (widget.userId != null && widget.postId == null) {
-              if (index == 0) {
-                return ContextPage();
-              }
-              index--;
-            }
-          } else {
-        
                   Text(
                     'Notifications',
                     style: titleTextStyle,
