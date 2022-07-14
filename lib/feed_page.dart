67edefb6ba9e56b4e77586643f9dd6d655be0099
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-14 13:16:40 +0200

    Update feed_page.dart

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -450,13 +450,6 @@ class _FeedPageState extends State<FeedPage> {
                 ),
               );
             }
-            index--;
-          }
-/*         if (index == 0) {
-            return Text(widget.userId.toString());
-          }
-           */
-
           
           } else {
             if (widget.userId == null && !rd.isNotificationsPage) {
